f002805ca58f985d3c50769926e04949e07c6d82
Pierre Kircher <pkircher@pk-level3.local>
2013-06-18 01:03:39 +0100

    changed log controller

--- app/controllers/passbook/logs_controller.rb
+++ app/controllers/passbook/logs_controller.rb
@@ -19,14 +19,14 @@ module Passbook
   class LogsController < ApplicationController
 
     def log
-      # if request && request.body
-      #   request.body.rewind
-      #   json_body = JSON.parse(request.body.read)
-      #   File.open(File.expand_path(Rails.root) + "/log/devices.log", "a") do |f|
-      #     f.write "[#{Time.now}] #{json_body["description"]}\n"
-      #   end
-      # end
-        render :nothing => true, :status => 200
+       if request && request.body
+         request.body.rewind
+         json_body = JSON.parse(request.body.read)
+         File.open(File.expand_path(Rails.root) + "/log/devices.log", "a") do |f|
+           f.write "[#{Time.now}] #{json_body["description"]}\n"
+        end
+      end
+      render :nothing => true, :status => 200
     end
 
   end #Class
