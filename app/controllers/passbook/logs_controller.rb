8eadb8980ac259edcd727469e5592b9356e03d0c
Pierre Kircher <pkircher@pk-level3.local>
2013-06-18 01:32:07 +0100

    changed log

--- app/controllers/passbook/logs_controller.rb
+++ app/controllers/passbook/logs_controller.rb
@@ -23,10 +23,10 @@ module Passbook
          request.body.rewind
          json_body = JSON.parse(request.body.read)
          File.open(File.expand_path(Rails.root) + "/log/devices.log", "a") do |f|
-           f.write "[#{Time.now}] #{json_body["description"]}\n"
+           f.write "[#{Time.now}] #{json_body["logs"]}\n"
         end
       end
-      render :nothing => true, :status => 200
+        render :nothing => true, :status => 200
     end
 
   end #Class
